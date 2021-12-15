f99cd3b706f2928debdec62e273828049367c86e
Jeong Pak <dannypak716@gmail.com>
2021-12-15 17:35:11 -0500

    finished authorization middleware so only users with admin role can pull user information by id

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -42,6 +42,11 @@ const only = role_name => (req, res, next) => {
 
     Pull the decoded token from the req object, to avoid verifying it again!
   */
+  if (role_name === req.decoded.role_name) {
+    next()
+  } else {
+    next({ status: 403, message: "This is not for you" })
+  }
 }
 
 
@@ -56,7 +61,7 @@ const checkUsernameExists = async (req, res, next) => {
   try {
     const [user] = await findBy({ username: req.body.username })
     if (!user) {
-      next({ status: 422, message: 'Invalid credentials' })
+      next({ status: 401, message: 'Invalid credentials' })
     } else {
       req.user = user
       next()
